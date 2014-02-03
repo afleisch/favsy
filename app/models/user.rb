5f76d950338f0d30df116149fbddfdbf3c4a5b56
afleisch <afleisch@gmail.com>
2014-02-03 15:30:47 -0800

    added #remember_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	before_save :create_remember_token
 	has_many :things
+
+
+
+
+	private
+
+	def create_remember_token
+		self.remember_token = SecureRandom.urlsafe_base64
+	end
+
+
+
+
+
 end
