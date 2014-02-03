b4457b8f40b086ddfa47c3927d62ed7500c98aa7
afleisch <afleisch@gmail.com>
2014-02-03 13:58:56 -0800

    added signin signup and signout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Favsy::Application.routes.draw do
   root to: "sites#index"
   resources :users, :sessions, :things
+
+get '/signup' => 'users#new'
+delete '/signout', to: 'sessions#destroy'
+get'/signin' => 'sessions#new'
   
 end
