bfa8720ea6c033f3b3ce0b85eeff3ccd3c027bde
afleisch <afleisch@gmail.com>
2014-02-03 21:31:23 -0800

    added #check_thing_owner

--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -1,2 +1,9 @@
 module ThingsHelper
+	def check_thing_owner
+		thing = current_user.things.where(:id => params[:id]).first
+		if thing.nil?
+		flash[:error] = "That's not your thing."
+			redirect_to thing_path(params[:id])
+		end
+	end	
 end
