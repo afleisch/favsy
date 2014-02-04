e2b8cc6b9d184210ed0ae1ebeb01b911bedfae2c
afleisch <afleisch@gmail.com>
2014-02-04 14:10:07 -0800

    added email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 	validates :password, confirmation: true
 	validates :email, confirmation: true
 	validates :email, uniqueness: { case_sensitive: false }
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	
 
 
