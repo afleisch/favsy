4778f5cc8b1cfd44fe2d892fb6a13ed0caf24f2a
afleisch <afleisch@gmail.com>
2014-02-04 12:29:31 -0800

    adjusted check_thing_owner to get correct params

--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -1,6 +1,6 @@
 module ThingsHelper
 	def check_thing_owner
-		thing = current_user.things.where(:id => params[:id]).first
+		thing = current_user.things.find(params[:id])
 		if thing.nil?
 		flash[:error] = "That's not your thing."
 			redirect_to thing_path(params[:id])
