87dc5b800570ed9ee9a461dee63f89da471fdd70
afleisch <afleisch@gmail.com>
2014-02-04 17:25:37 -0800

    fixed indentation in things helper

--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -7,17 +7,16 @@ module ThingsHelper
 		end
 	end	
 
-
 	def get_address
       id = params[:id]
       @thing = Thing.find(id)
       @address = @thing.street + ", " + @thing.city + ", " + @thing.state + ", " + @thing.zip_code
       return @address
-    end
+  end
 
-    def get_map_url
-    	@address = get_address
-    	@map_url = "https://maps.googleapis.com/maps/api/staticmap?markers=color:red%7Clabel:C%7C#{@address}&zoom=13&size=300x300&sensor=false"
+  def get_map_url
+    @address = get_address
+    @map_url = "https://maps.googleapis.com/maps/api/staticmap?markers=color:red%7Clabel:C%7C#{@address}&zoom=13&size=300x300&sensor=false"
     
-    end
+  end
 end
