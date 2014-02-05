7aa06fd96b66eed127687746549b00c0f66489df
afleisch <afleisch@gmail.com>
2014-02-04 18:01:15 -0800

    tried to add validation to thing

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,3 +1,8 @@
 class Thing < ActiveRecord::Base
 	belongs_to :user
+	# validates :state, length: { is: 2, 
+	# too_short: "Please enter %{count}-character state abbreviation" }
+	# validates :zip_code, length: { is: 5 }
+	# validates_length_of :password, :in => 6..15, :on => :create
+	
 end
