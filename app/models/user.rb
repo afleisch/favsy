f088a98d4c202dd00c71c6240fd3e3b5f14f48d0
afleisch <afleisch@gmail.com>
2014-02-04 17:10:46 -0800

    added minimum password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 	validates :email, confirmation: true
 	validates :email, uniqueness: { case_sensitive: false }
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	validates_length_of :password, :in => 6..15, :on => :create
 	
 
 
