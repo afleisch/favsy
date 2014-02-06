e44d48e3e32829730f67052a72c320557998d20e
afleisch <afleisch@gmail.com>
2014-02-05 17:38:11 -0800

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140202223934) do
+ActiveRecord::Schema.define(version: 20140203231503) do
 
   create_table "things", force: true do |t|
     t.string   "name"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20140202223934) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,8 @@
 
-ashley = User.create(email: "ashley@gmail.com", password: "asdf", password_confirmation: "asdf")
-# User.create(email: "lauren@gmail.com", password: "rosie", password_confirmation: "rosie")
+ashley = User.create(email: "ashley@gmail.com", password: "asdfgh", password_confirmation: "asdfgh")
+lauren = User.create(email: "lauren@gmail.com", password: "rosie1", password_confirmation: "rosie1")
 # User.create(email: "linda@gmail.com", password: "frank", password_confirmation: "frank")
 
-ashley.things.create(name: "Pork Slope", description: "Restaurant", 
-	reason: "delicious BBQ, great bar, has pool table", street: "247 5th Avenue", 
-	city: "Brooklyn", state: "NY", zip_code: "11215")
 
 ashley.things.create(name: "Pork Slope", description: "Restaurant", 
 reason: "delicious BBQ, great bar, has pool table", street: "247 5th Avenue", 
@@ -18,10 +15,12 @@ ashley.things.create(name: "Clooney's", description: "Bar", reason: "local dive 
 city: "San Francisco", state: "CA", zip_code: "94110")
 ashley.things.create(name: "smallfrys", description: "Store", reason: "great baby clothes for Rosie", 
 street: "4066 24th Street", city: "San Francisco", state: "CA", zip_code: "94114")
-ashley.things.create(name: "MUNI", description: "Transportation", reason: "the WORST public transporation system, takes forever and goes nowhere", street: "1 S Van Ness Avenue",
-city: "San Francisco", state: "CA", zip_code: "94103")
-ashley.things.create(name: "Ten Sushi", description: "Restaurant", reason: "they don't use fresh fish, found a scale in tuna", 
-street: "440 7th Avenue", city: "Brooklyn", state: "NY", zip_code: "11215")
+
+lauren.things.create(name: "Bette", description: "beauty", reason: "stylist who can cut and style thick hair",
+	street: "3915 24th Street", city: "San Francisco", state: "CA", zip_code: "94114")
+lauren.things.create(name: "Tea Way", description: "cafe", reason: "great bubble tea, has good variety of boba",
+	street: "2150 Irving Avenue", city: "San Francisco", state: "CA", zip_code: "94122")
+
 
 
 
