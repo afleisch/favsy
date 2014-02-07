be18850a321f70b849ca0e2c9fcf55a483775994
afleisch <afleisch@gmail.com>
2014-02-06 16:00:42 -0800

    added state length validation

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,6 +1,6 @@
 class Thing < ActiveRecord::Base
 	belongs_to :user
-	# validates :state, length: { is: 2, 
+	validates_length_of :state, is: 2, :on => :create
 	# too_short: "Please enter %{count}-character state abbreviation" }
 	# validates :zip_code, length: { is: 5 }
 	# validates_length_of :password, :in => 6..15, :on => :create
