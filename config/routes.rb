f47cdfba651dff513f1fe2a4f5164951a3aac2e4
afleisch <afleisch@gmail.com>
2014-02-06 18:55:53 -0800

    adjust spacing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Favsy::Application.routes.draw do
   root to: "sites#index"
   resources :users, :sessions, :things
 
-get '/signup' => 'users#new'
-delete '/signout', to: 'sessions#destroy'
-get'/signin' => 'sessions#new'
+  get '/signup' => 'users#new'
+  delete '/signout', to: 'sessions#destroy'
+  get'/signin' => 'sessions#new'
   
 end
