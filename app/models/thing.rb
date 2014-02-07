c843649a93998488c01249a97ed7a6f839394a6d
afleisch <afleisch@gmail.com>
2014-02-06 16:47:43 -0800

    deleted comments

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,8 +1,7 @@
 class Thing < ActiveRecord::Base
 	belongs_to :user
 	validates_length_of :state, is: 2, :on => :create
-	# too_short: "Please enter %{count}-character state abbreviation" }
 	validates_length_of :zip_code, is: 5, :on => :create
-	# validates_length_of :password, :in => 6..15, :on => :create
+	
 	
 end
